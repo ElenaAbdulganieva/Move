62ca0e59fc9dc42eb060b115986baea32f5e385d
ElenaAbdulganieva <82102039+ElenaAbdulganieva@users.noreply.github.com>
2021-05-30 20:46:02 +0400

    Разделение на функции, работа с указателями

--- move.cpp
+++ move.cpp
@@ -1,34 +1,30 @@
-#include "TXlib.h"
-
-void Management (int vx, int vy);
+#include "TxLib.h"
 
 void DrawStar (int x, int y, double sizeX, double sizeY);
 
-void StarManagement
-(int x, int y, double sizeX, double sizeY, int vx, int vy, int dt,
- int leftborder, int rightborder, int upborder, int downborder);
+void DrawStarBlack (int x, int y, double sizeX, double sizeY);
+
+void Move (int* x, int* y, double sizeX, double sizeY, int* vx, int* vy, int dt,
+           int leftborder, int rightborder, int upborder, int downborder);
+
+void Management (int* vx, int* vy);
+
+void StarMoveManagement ();
 
 int main ()
     {
     txCreateWindow (800, 600);
 
-    StarManagement (200, 200, 0.3, 0.3, 5, 5, 1, 10, 790, 10, 590);
+    StarMoveManagement ();
 
     return 0;
     }
 
-void Management (int vx, int vy)
-    {
-    if (txGetAsyncKeyState (VK_RIGHT)) vx ++;
-    if (txGetAsyncKeyState (VK_LEFT))  vx --;
-    if (txGetAsyncKeyState (VK_UP))    vy --;
-    if (txGetAsyncKeyState (VK_DOWN))  vy ++;
-
-    if (txGetAsyncKeyState (VK_SPACE)) vx = vy = 0;
-    }
-
 void DrawStar (int x, int y, double sizeX, double sizeY)
     {
+    txSetColor (RGB (x + y, y, x), 5);
+    txSetFillColor (RGB (x, y, x + y));
+
     POINT star [] = {{x              , y - 164 * sizeY},
                      {x +  34 * sizeX, y -  52 * sizeY},
                      {x + 150 * sizeX, y -  50 * sizeY},
@@ -43,49 +39,83 @@ void DrawStar (int x, int y, double sizeX, double sizeY)
     txPolygon (star, 11);
     }
 
-void StarManagement
-(int x, int y, double sizeX, double sizeY, int vx, int vy, int dt,
- int leftborder, int rightborder, int upborder, int downborder)
+void DrawStarBlack (int x, int y, double sizeX, double sizeY)
     {
+    txSetColor (TX_BLACK, 5);
+    txSetFillColor (TX_BLACK);
+
+    POINT star [] = {{x              , y - 164 * sizeY},
+                     {x +  34 * sizeX, y -  52 * sizeY},
+                     {x + 150 * sizeX, y -  50 * sizeY},
+                     {x +  59 * sizeX, y +  17 * sizeY},
+                     {x +  92 * sizeX, y + 131 * sizeY},
+                     {x              , y +  65 * sizeY},
+                     {x -  92 * sizeX, y + 131 * sizeY},
+                     {x -  59 * sizeX, y +  17 * sizeY},
+                     {x - 150 * sizeX, y -  50 * sizeY},
+                     {x -  34 * sizeX, y -  52 * sizeY},
+                     {x              , y - 164 * sizeY}};
+    txPolygon (star, 11);
+    }
+
+void Move (int* x, int* y, double sizeX, double sizeY, int* vx, int* vy, int dt,
+           int leftborder, int rightborder, int upborder, int downborder)
+    {
+    *x += (*vx) * dt;
+    *y += (*vy) * dt;
+
+    if (*x > rightborder - 165 * sizeX)
+        {
+        *vx = - (*vx);
+        *x = rightborder - 165 * sizeX;
+        }
+
+    if (*y > downborder - 165 * sizeX)
+        {
+        *vy = - (*vy);
+        *y = downborder - 165 * sizeX;
+        }
+
+    if (*x < leftborder + 165 * sizeX)
+        {
+        *vx = - (*vx);
+        *x = leftborder + 165 * sizeX;
+        }
+
+    if (*y < upborder + 165 * sizeX)
+        {
+        *vy = - (*vy);
+        *y = upborder + 165 * sizeX;
+        }
+    }
+
+void Management (int* vx, int* vy)
+    {
+    if (txGetAsyncKeyState (VK_RIGHT)) (*vx) ++;
+    if (txGetAsyncKeyState (VK_LEFT))  (*vx) --;
+    if (txGetAsyncKeyState (VK_UP))    (*vy) --;
+    if (txGetAsyncKeyState (VK_DOWN))  (*vy) ++;
+
+    if (txGetAsyncKeyState (VK_SPACE)) *vx = *vy = 0;
+    }
+
+void StarMoveManagement ()
+    {
+    int x = 100; int y = 100;
+    double sizeX = 0.3; double sizeY = 0.3;
+    int vx = 5; int vy = 5;
+    int dt = 1;
+    int leftborder = 10; int rightborder = 790;
+    int upborder = 10; int downborder = 590;
+
     while (! txGetAsyncKeyState (VK_ESCAPE))
         {
-        txSetColor (RGB (x + y, y, x), 5);
-        txSetFillColor (RGB (x, y, x + y));
         DrawStar (x, y, sizeX, sizeY);
-
         txSleep (20);
+        DrawStarBlack (x, y, sizeX, sizeY);
 
-        txSetColor (TX_BLACK, 5);
-        txSetFillColor (TX_BLACK);
-        DrawStar (x, y, sizeX, sizeY);
-
-        x += vx * dt;
-        y += vy * dt;
-
-        if (x > rightborder - 165 * sizeX)
-            {
-            vx = - vx;
-            x = rightborder - 165 * sizeX;
-            }
-
-        if (y > downborder - 165 * sizeX)
-            {
-            vy = - vy;
-            y = downborder - 165 * sizeX;
-            }
-
-        if (x < leftborder + 165 * sizeX)
-            {
-            vx = - vx;
-            x = leftborder + 165 * sizeX;
-            }
-
-        if (y < upborder + 165 * sizeX)
-            {
-            vy = - vy;
-            y = upborder + 165 * sizeX;
-            }
-
-        Management (vx, vy);
+        Move (&x, &y, sizeX, sizeY, &vx, &vy, dt,
+              leftborder, rightborder, upborder, downborder);
+        Management (&vx, &vy);
         }
     }
